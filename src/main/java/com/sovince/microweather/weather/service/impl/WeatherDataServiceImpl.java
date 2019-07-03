f0ce73c0c49dd3d03b2cb6301618a4aa9d3529a4
Vince <so_vince@outlook.com>
2019-07-03 20:56:43 +0800

    optimizing api by redis

--- src/main/java/com/sovince/microweather/weather/service/impl/WeatherDataServiceImpl.java
+++ src/main/java/com/sovince/microweather/weather/service/impl/WeatherDataServiceImpl.java
@@ -3,7 +3,11 @@ package com.sovince.microweather.weather.service.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sovince.microweather.weather.service.WeatherDataService;
 import com.sovince.microweather.weather.vo.WeatherResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,10 +23,15 @@ import java.io.IOException;
  */
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini";
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
@@ -37,19 +46,34 @@ public class WeatherDataServiceImpl implements WeatherDataService {
     }
 
     private WeatherResponse doGetWeather(String uri) {
-        ResponseEntity<String> entity = restTemplate.getForEntity(uri, String.class);//不能直接转pojo?
+        String key = "weather:"+uri;
+        //redis crud句柄
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
         WeatherResponse weatherResponse = null;
-        ObjectMapper objectMapper = new ObjectMapper();
+        String body = null;
+        if(stringRedisTemplate.hasKey(key)){
+            logger.info("Getting data from redis...");
+            body = ops.get(key);
+        }else{
+            logger.info("Getting data from uri...");
+            ResponseEntity<String> entity = restTemplate.getForEntity(uri, String.class);//不能直接转pojo?
+            if (entity.getStatusCodeValue() == 200) {
+                body = entity.getBody();
+            }
+            ops.set(key,body);
+        }
 
-        if (entity.getStatusCodeValue() == 200) {
-            String body = entity.getBody();
+        if(body!=null){
             try {
+                ObjectMapper objectMapper = new ObjectMapper();
                 weatherResponse = objectMapper.readValue(body, WeatherResponse.class);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
+
+
+
         return weatherResponse;
     }
 }
