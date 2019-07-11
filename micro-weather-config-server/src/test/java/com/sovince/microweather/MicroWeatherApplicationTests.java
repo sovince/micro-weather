58cd19643f875ce46f6c0ea688f6b4258be64770
Vince <so_vince@outlook.com>
2019-07-11 08:58:26 +0800

    test cloud-config

--- micro-weather-config-server/src/test/java/com/sovince/microweather/MicroWeatherApplicationTests.java
+++ micro-weather-config-server/src/test/java/com/sovince/microweather/MicroWeatherApplicationTests.java
@@ -2,7 +2,6 @@ package com.sovince.microweather;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,21 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MicroWeatherApplicationTests {
 
-    @Value("auther")
-    private String auther;
-
     @Test
     public void contextLoads() {
-
-    }
-
-    /**
-     * 测试cloud-config
-     */
-    @Test
-    public void foo(){
-        //git中的配置
-        System.out.println(auther);
     }
 
 }
